ab3932cb5dc7b582483e6aa66ac5aa35ed7413d7
peterivanic <peterivanic89@gmail.com>
2020-02-07 12:15:14 +0100

    Make Circle

---
+++ src/src/Circle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+
+        System.out.print("Write radius : ");
+
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+
+       double circumference =  Math.PI * (radius*2);
+       double content = radius * radius * Math.PI;
+
+        System.out.println("Circumference this circle is : " + circumference + " cm");
+        System.out.println("Circle content this circle is : " + content + " cm2");
+
+
+
+    }
+}
