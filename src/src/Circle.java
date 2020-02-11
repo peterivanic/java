3236c04e1c2210711fff2910e44a9fd54f5c6aa0
peterivanic <peterivanic89@gmail.com>
2020-02-11 08:50:07 +0100

    Make Circle

--- src/src/Circle.java
+++ src/src/Circle.java
@@ -6,15 +6,12 @@ public class Circle {
         System.out.print("Write radius : ");
 
         Scanner sc = new Scanner(System.in);
-        double radius = sc.nextDouble();
+        double radius = Double.parseDouble (sc.nextLine());
 
        double circumference =  Math.PI * (radius*2);
        double content = radius * radius * Math.PI;
 
         System.out.println("Circumference this circle is : " + circumference + " cm");
         System.out.println("Circle content this circle is : " + content + " cm2");
-
-
-
     }
 }
