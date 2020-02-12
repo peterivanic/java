31251b696620d2cf8db97ac32c2a9a00959fc096
peterivanic <peterivanic89@gmail.com>
2020-02-12 18:50:32 +0100

    Make Ascii

---
+++ src/src/Ascii.java
@@ -0,0 +1,8 @@
+public class Ascii {
+    public static void main(String[] args) {
+        for (int i = 33 ; i <= 255 ; i++){
+            char c =(char)i;
+            System.out.println(i + ":" + c );
+        }
+    }
+}
